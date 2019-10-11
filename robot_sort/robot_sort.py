a90ec4e3d98705dcd2af5ecb91a16ff1a46814a5
hectorsvill <hectorstevenvillasano@gmail.com>
2019-10-11 14:21:59 -0700

    sort 2

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -100,7 +100,7 @@ class SortingRobot:
 
     def sort_2(self):
         self.set_light_off()
-        # self.set_item()
+        self.set_item()
 
 
         while (self.can_move_right()):
@@ -108,7 +108,7 @@ class SortingRobot:
             
             if self.compare_item() == -1: #self._list[self._position] > self._list[self._position + 1]:
                 print(f"current iterm: {self._item}\t {self.compare_item()}")
-                self.swap_item()  # self._list[self._position], self._list[self._position + 1] = self._list[self._position + 1],  self._list[self._position] 
+                self.swap_item  # self._list[self._position], self._list[self._position + 1] = self._list[self._position + 1],  self._list[self._position] 
                 self.set_light_on()
                 
             
@@ -160,7 +160,7 @@ if __name__ == "__main__":
     l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
     small_list = [5, 4, 3, 2, 1]
        
-    robot = SortingRobot(small_list)
+    robot = SortingRobot(l)
 
     robot.sort_2()
     
